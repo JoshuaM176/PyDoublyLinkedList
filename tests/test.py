1be9a2b0a30f654626061d1b9702bb72abecf4a0
JoshuaM176 <joshuam18745@gmail.com>
2025-10-21 12:09:28 -0500

    update tests

--- tests/test.py
+++ tests/test.py
@@ -24,10 +24,10 @@ def test_length():
 
 def test_node_refs(): # Most changes to ref counts happen in the same two internal functions, so no need to thoroughly test every method
 	test_list = create_test_list(); assert sys.getrefcount(test_list.tail) == 4
-	assert sys.getrefcount(test_list.head) == 3
+	assert sys.getrefcount(test_list.head) == 2
 	head = test_list.head; del test_list[0]; assert sys.getrefcount(head) == 2
 	assert sys.getrefcount(test_list.tail) == 3
-	node1 = test_list.head.next; assert sys.getrefcount(node1) == 4
+	node1 = test_list.head.next; assert sys.getrefcount(node1) == 3
 	test_list.pop(1); assert sys.getrefcount(node1) == 2
 
 def test_cursor_pos():
