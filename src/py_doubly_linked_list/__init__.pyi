347dd08871bdaa73f3924c92fe8af66300023a36
Joshua Morningstar <joshuam18745@gmail.com>
2025-12-14 00:15:49 -0600

    correction on typing

--- src/py_doubly_linked_list/__init__.pyi
+++ src/py_doubly_linked_list/__init__.pyi
@@ -1,4 +1,4 @@
-from typing import Iterable, SupportsIndex, TypeVar, overload
+from typing import Iterable, TypeVar, overload
 from collections.abc import Callable, MutableSequence
 from _typeshed import SupportsRichComparison, SupportsRichComparisonT, _T
 import sys
@@ -24,14 +24,14 @@ class DoublyLinkedList(MutableSequence[_T]):
     def extend(self, iterable: Iterable[_T], forward: bool = True) -> None:
         """Extend list by appending elements from the iterable. Set forward to false to extend from the start."""
         ...
-    def index(self, value: _T, start: SupportsIndex = 0, stop: SupportsIndex = sys.maxsize) -> int:
+    def index(self, value: _T, start: int = 0, stop: int = sys.maxsize) -> int:
         """Return first index of value.  
         Raises ValueError if the value is not present."""
         ...
-    def insert(self, object: _T, index: SupportsIndex, forward: bool = True) -> None:
+    def insert(self, object: _T, index: int, forward: bool = True) -> None:
         """Insert object after index. Set forward to false to insert before index."""
         ...
-    def pop(self, index: SupportsIndex = -1) -> _T:
+    def pop(self, index: int = -1) -> _T:
         """Remove and return item at index (default last).  
         Raises IndexError if list is empty or index is out of range."""
         ...
